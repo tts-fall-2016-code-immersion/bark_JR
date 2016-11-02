b4040b3ccedc22d8b66c657586888d2df7788015
Jessica Ransone <jranso21@gmail.com>
2016-11-02 16:54:21 -0400

    class on 11-1-16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
-
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
-    end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location, :email, :password, :password_confirmation])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 20161027204617) do
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
-  create_table "tweets", force: :cascade do |t|
-    t.string   "message"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tweets_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
