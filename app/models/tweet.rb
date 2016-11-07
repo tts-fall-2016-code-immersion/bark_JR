04218136ee3a8e34399128753485b1701d0f67e5
Jessica Ransone <jranso21@gmail.com>
2016-11-07 17:46:56 -0500

    for class on 11-7-16

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :tweets
+
+validates :message, presence: true, length: {maximum: 140, too_long: "A tweet is only 140 characters! Everybody knows that!"}
+
 end
